f749517885d7647744f90a36501ed43eaf35b01a
smyrcvlk <smyrcvlk@gmail.com>
2019-11-17 16:25:43 -0500

    car

--- src/car.java
+++ src/car.java
@@ -1,5 +1,11 @@
 public class car {
-    public static void main(String[] args) {
-        System.out.println("car");
-    }
+  public String name;
+  public int speed;
+  public String engine;
+  public int door;
+  public String color;
+
+  public void drive(){
+      
+  }
 }
